fc4c0a08e2b29b63f719d5054030501b504b3b80
b0nnie-7 <113034423+b0nnie-7@users.noreply.github.com>
2022-09-27 09:10:52 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,5 +7,8 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     counter += -1
 })
+// Create by BonnieZhu
+// Create on Sep 27th
+// This program is variables
 let counter = 0
 counter = 0
